b691339e4644c0cd35948a7302dcf2a47ff66fba
Hamid Saadi <saadi.hamid@gmail.com>
2025-12-17 15:16:47 +0330

    Create fruits.py

---
+++ fruits.py
@@ -0,0 +1,7 @@
+# برنامه ایی بنویسید که رنگ میوه های لیست را نمایش دهد
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x=="apple" or x=="cherry":
+    print("red")
+  elif x=="banana":
+    print("Yellow")
